807ea6a8b772c220cf983d25b36a84da8c8dc036
lfbrehm <97600985+lfbrehm@users.noreply.github.com>
2024-09-04 13:14:34 +0200

    feat: v2/v3 separation

--- tests/build.rs
+++ tests/build.rs
@@ -10,49 +10,61 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn compile_services() -> Result<(), Box<dyn std::error::Error>> {
     let mut protos: Vec<String> = Vec::new();
 
-    let service_entries = fs::read_dir("aruna/api/dataproxy/services/v2/")?;
+    let service_entries = fs::read_dir("v2/aruna/api/dataproxy/services/v2/")?;
 
     for entry in service_entries {
         let dir = entry?;
         let rel_path = format!(
             "{}{}",
-            "aruna/api/dataproxy/services/v2/",
+            "v2/aruna/api/dataproxy/services/v2/",
             dir.file_name().to_str().unwrap().to_string()
         );
         protos.push(rel_path);
     }
 
-    let service_entries = fs::read_dir("aruna/api/hooks/services/v2/")?;
+    let service_entries = fs::read_dir("v2/aruna/api/hooks/services/v2/")?;
 
     for entry in service_entries {
         let dir = entry?;
         let rel_path = format!(
             "{}{}",
-            "aruna/api/hooks/services/v2/",
+            "v2/aruna/api/hooks/services/v2/",
             dir.file_name().to_str().unwrap().to_string()
         );
         protos.push(rel_path);
     }
 
-    let service_entries = fs::read_dir("aruna/api/notification/services/v2/")?;
+    let service_entries = fs::read_dir("v2/aruna/api/notification/services/v2/")?;
 
     for entry in service_entries {
         let dir = entry?;
         let rel_path = format!(
             "{}{}",
-            "aruna/api/notification/services/v2/",
+            "v2/aruna/api/notification/services/v2/",
             dir.file_name().to_str().unwrap().to_string()
         );
         protos.push(rel_path);
     }
 
-    let service_entries = fs::read_dir("aruna/api/storage/services/v2/")?;
+    let service_entries = fs::read_dir("v2/aruna/api/storage/services/v2/")?;
 
     for entry in service_entries {
         let dir = entry?;
         let rel_path = format!(
             "{}{}",
-            "aruna/api/storage/services/v2/",
+            "v2/aruna/api/storage/services/v2/",
+            dir.file_name().to_str().unwrap().to_string()
+        );
+        protos.push(rel_path);
+    }
+
+    let v3_entries = fs::read_dir("v3/")?;
+
+    for entry in v3_entries {
+        let dir = entry?;
+        let rel_path = format!(
+            "{}{}",
+            "v3/",
             dir.file_name().to_str().unwrap().to_string()
         );
         protos.push(rel_path);
@@ -65,8 +77,8 @@ fn compile_services() -> Result<(), Box<dyn std::error::Error>> {
             &protos,
             &[
                 "./".to_string(),
-                "aruna/api/google".to_string(),
-                "aruna/api/protoc-gen-openapiv2".to_string(),
+                "v2/aruna/api/google".to_string(),
+                "v2/aruna/api/protoc-gen-openapiv2".to_string(),
             ],
         )
         .unwrap();
